8b3599b4a5ccc41de1642c132e2184760246bb3f
Bennett Buchanan <bennett.buchanan@scality.com>
2017-01-19 15:27:01 -0800

    [squash] Add service route

--- src/handlers/metricsHandlers.js
+++ src/handlers/metricsHandlers.js
@@ -9,6 +9,7 @@ export class BucketsHandler {
     /**
     * List metrics for the given list of buckets
     * @param {UtapiRequest} utapiRequest - UtapiRequest instance
+    * @param {string} component - the service component (e.g., 's3')
     * @param {callback} cb - callback
     * @return {undefined}
     */
@@ -31,6 +32,7 @@ export class AccountsHandler {
     /**
     * List metrics for the given list of accounts
     * @param {UtapiRequest} utapiRequest - UtapiRequest instance
+    * @param {string} component - the service component (e.g., 's3')
     * @param {callback} cb - callback
     * @return {undefined}
     */
@@ -40,6 +42,29 @@ export class AccountsHandler {
             method: 'AccountsHandler.listMetrics',
         });
         const Accounts = new ListMetrics('accounts', component);
+        return Accounts.getTypesMetrics(utapiRequest, cb);
+    }
+}
+
+/**
+ * @class ServiceHandler
+ * Handles Accounts resource actions
+ */
+
+export class ServiceHandler {
+    /**
+    * List metrics for the given list of accounts
+    * @param {UtapiRequest} utapiRequest - UtapiRequest instance
+    * @param {string} component - the service component (e.g., 's3')
+    * @param {callback} cb - callback
+    * @return {undefined}
+    */
+    static listMetrics(utapiRequest, component, cb) {
+        const log = utapiRequest.getLog();
+        log.debug('handling list metrics request', {
+            method: 'ServiceHandler.listMetrics',
+        });
+        const Accounts = new ListMetrics('service', component);
         return Accounts.getTypesMetrics(utapiRequest, cb);
     }
 }
